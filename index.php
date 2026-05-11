912d6fdfc0614762bccb75bf5854ee4dc8b77667
Bruno <brunosmerlo@gmail.com>
2026-05-11 18:16:11 -0300

    Se agrega la vista de detalle del pokemon (US-04)

--- index.php
+++ index.php
@@ -16,54 +16,73 @@ $iconosTipos = [
 
 ?>
 
-<!doctype html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Pokédex</title>
-</head>
-<body>
+    <!doctype html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+              integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+              crossorigin="anonymous">
+        <link rel="stylesheet" href="css/style.css">
+        <title>Pokédex</title>
+    </head>
+    <body>
 
 
-<main class="container mt-5">
-    <h1 class="text-center mb-4">Mi Pokédex</h1>
+    <main class="container mt-5">
+        <h1 class="text-center mb-4">Mi Pokédex</h1>
+        <?php
+        if (isset($_SESSION['error']) && $_SESSION['error'] == 'no_existe') {
+
+            echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>El Pokémon que buscás no existe.
+                   </div>";
+            unset($_SESSION['error']);
+        }
+        ?>
+
+        <div class="row">
+
+            <?php while ($pokemon = $resultado->fetch_assoc()) : ?>
 
 
-    <div class="row">
+                <div class="col-md-4 col-12 mb-4">
 
-        <?php while ($pokemon = $resultado->fetch_assoc()) : ?>
+                    <div class="card h-100 shadow-sm">
 
+                        <img src="<?php echo $pokemon['imagen_ruta']; ?>" class="card-img-top"
+                             alt="<?php echo $pokemon['nombre']; ?>">
 
-            <div class="col-md-4 col-12 mb-4">
+                        <div class="card-body text-center">
 
-                <div class="card h-100 shadow-sm">
-
-                    <img src="<?php echo $pokemon['imagen_ruta']; ?>" class="card-img-top"
-                         alt="<?php echo $pokemon['nombre']; ?>">
-
-                    <div class="card-body text-center">
-
-                        <h5 class="card-title"><?php echo $pokemon['nombre']; ?></h5>
+                            <h5 class="card-title"><?php echo $pokemon['nombre']; ?></h5>
 
 
-                        <p class="card-text">Tipo:<img src="assets/tipos/<?php echo $iconosTipos[$pokemon['tipo']]; ?>"
-                                                       alt="<?php echo $pokemon['tipo']; ?>" width="20"></p>
+                            <p class="card-text">Tipo:<img
+                                        src="assets/tipos/<?php echo $iconosTipos[$pokemon['tipo']]; ?>"
+                                        alt="<?php echo $pokemon['tipo']; ?>" width="20">
+                            </p>
+
+                            <a href="detalle.php?id=<?php echo $pokemon['id']; ?>" class="btn btn-primary mt-3 w-100">Ver
+                                detalle</a>
+
+                        </div>
                     </div>
+
+
                 </div>
 
+            <?php endwhile; ?>
 
-            </div>
-
-        <?php endwhile; ?>
-
-    </div>
-</main>
+        </div>
+    </main>
 
 
-</body>
-</html>
+    </body>
+    </html>
+
+<?php
+$conexion->close();
+?>
